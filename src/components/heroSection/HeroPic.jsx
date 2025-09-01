fd23c595d08b49bd60e9a84503c4e9033674407a
Nirman <nirmanraj0512@gmail.com>
2025-09-02 00:00:46 +0530

    Final Push

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -11,7 +11,7 @@ const  HeroPic=()=>{
             whileInView="show"
             viewport={{once:false,amount:0}}
     className='h-full flex items-center justify-center'>
-        <img src="../../Image/my.png" alt='Nirman Raj' className='max-h-[450px] w-auto '/>
+        <img src="../../Image/NR1.png" alt='Nirman Raj' className='max-h-[450px] w-auto '/>
         <div className='absolute -z-10 flex justify-center items-center animate-pulse'>
             <PiHexagonThin className='md:h-[90%] sm:h-[120%] min-h-[600px] w-auto text-cyan blur-md animate-[spin_20s_linear_infinite]'/>
         </div>
